8934be160f635fd243b27b2a27acaf606ea97001
woodoo <wooodward77@gmail.com>
2023-12-04 13:11:19 +0300

    comment added

--- src/main/java/org/testtask/testtask1/BracketsChecker.java
+++ src/main/java/org/testtask/testtask1/BracketsChecker.java
@@ -1,5 +1,8 @@
 package org.testtask.testtask1;
-
+/*
+* В метод приходит строка, необходимо проверить ее на сбалансированность скобок,
+* т.е. каждой открывающей скобке соответствует закрывающая.
+* */
 public class BracketsChecker {
     public boolean isBalanced(String input){
         return false;
