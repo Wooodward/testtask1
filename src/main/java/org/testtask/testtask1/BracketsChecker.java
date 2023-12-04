a2d49e9922d40e98ee19f064beaac1ede1658e7b
woodoo <wooodward77@gmail.com>
2023-12-04 13:03:30 +0300

    initial commit

---
+++ src/main/java/org/testtask/testtask1/BracketsChecker.java
@@ -0,0 +1,8 @@
+package org.testtask.testtask1;
+
+public class BracketsChecker {
+    public boolean isBalanced(String input){
+        return false;
+    }
+
+}
---
+++ src/test/java/org/testtask/testtask1/BracketsCheckerTest.java
@@ -0,0 +1,40 @@
+package org.testtask.testtask1;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class BracketsCheckerTest {
+    private BracketsChecker bracketsChecker;
+
+    @BeforeMethod
+    public void setUp(){
+        bracketsChecker = new BracketsChecker();
+    }
+
+    @DataProvider
+    public Object[][] bracketsProvider(){
+        return new Object[][]{
+                {"()", true},
+                {"[]", true},
+                {"{}", true},
+                {"(()", false},
+                {"())", false},
+                {"[[]", false},
+                {"[]]", false},
+                {"{{}", false},
+                {"{}}", false},
+                {"([])", true},
+                {"[()]", true},
+                {"aaaa[dddd(ssss)cccc]", true},
+                {"aa[ss[dd(cc[vv)bb]nn]nnn", false}
+        };
+    }
+
+    @Test(dataProvider = "bracketsProvider")
+    public void testIsBalanced(String input, boolean expected){
+        assertEquals(bracketsChecker.isBalanced(input), expected);
+    }
+}
